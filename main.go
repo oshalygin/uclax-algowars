16a458b3d00d407970050a22eebc8802dd9aac8b
Oleg Shalygin <oshalygin@gmail.com>
2017-12-15 22:55:55 -0800

    Add base read template

--- main.go
+++ main.go
@@ -1,15 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 
 	"github.com/fatih/color"
 )
 
 func main() {
+
 	startMessage := "The start of something great"
 
 	color.Green(startMessage)
-	color.Green(strings.Repeat("=", len(startMessage)))
+	color.White(strings.Repeat("=", len(startMessage)))
 
+	var n int
+	io := bufio.NewReader(os.Stdin)
+	fmt.Fscan(io, &n)
+
+	arr := make([]int, n)
+
+	for i := 0; i < n; i++ {
+		fmt.Fscan(io, &arr[i])
+	}
+	fmt.Println(arr)
 }
