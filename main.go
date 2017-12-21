ec4381b32d8bab809106c23d9169adf60039ab39
Oleg Shalygin <oshalygin@gmail.com>
2017-12-20 23:29:02 -0800

    Add Diagonal difference solution

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 	"os"
 	"strings"
 
-	"github.com/fatih/color"
 	"github.com/oshalygin/uclax-algowars/solutions"
+
+	"github.com/fatih/color"
 )
 
 func main() {
@@ -21,12 +22,17 @@ func main() {
 	io := bufio.NewReader(os.Stdin)
 	fmt.Fscan(io, &n)
 
-	input := make([]int64, n)
-
+	input := make([][]int, n)
+	println(n)
 	for i := 0; i < n; i++ {
-		fmt.Fscan(io, &input[i])
+		subArray := make([]int, n)
+		for j := 0; j < n; j++ {
+			fmt.Fscan(io, &subArray[j])
+		}
+		input[i] = subArray
 	}
 
-	result := solutions.AVeryBigSum(input)
+	result := solutions.DiagonalDifference(input)
 	fmt.Println(result)
+
 }
