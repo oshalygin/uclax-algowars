b55b3cd7e522e2e556141103f32afdf4dd74a282
Oleg Shalygin <oshalygin@gmail.com>
2017-12-24 23:10:18 -0800

    Add initial camelcase solution

---
+++ solutions/camelcase_test.go
@@ -0,0 +1,12 @@
+package solutions_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/oshalygin/uclax-algowars/solutions"
+)
+
+var _ = Describe("Camelcase", func() {
+
+})
