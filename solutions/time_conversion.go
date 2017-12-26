34d41748927b0fd2f2476ec38082b7ec0d75594a
oshalygin <oshalygin@gmail.com>
2017-12-25 19:53:28 -0800

    Add TimeConversion Utility

---
+++ solutions/time_conversion.go
@@ -0,0 +1,49 @@
+/*
+
+Given a time in -hour AM/PM format, convert it to military (-hour) time.
+
+Note: Midnight is  on a -hour clock, and  on a -hour clock. Noon is  on a -hour clock, and  on a -hour clock.
+
+Input Format
+
+A single string containing a time in -hour clock format (i.e.:  or ), where  and .
+
+Output Format
+
+Convert and print the given time in -hour format, where .
+
+Sample Input
+
+07:05:45PM
+Sample Output
+
+19:05:45
+
+*/
+
+package solutions
+
+import (
+	"strconv"
+	"strings"
+)
+
+func TimeConversion(date string) string {
+	isPM := strings.Contains(date, "PM")
+
+	replacer := strings.NewReplacer("PM", "", "AM", "")
+	updated := replacer.Replace(date)
+
+	if !isPM {
+		return updated
+	}
+
+	splitTime := strings.Split(updated, ":")
+	hours, _ := strconv.Atoi(splitTime[0])
+	hours = hours + 12
+
+	splitTime[0] = strconv.Itoa(hours)
+
+	updated = strings.Join(splitTime, ":")
+  return updated
+}
---
+++ solutions/time_conversion_test.go
@@ -0,0 +1,19 @@
+package solutions_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/oshalygin/uclax-algowars/solutions"
+)
+
+var _ = Describe("TimeConversion", func() {
+	It("07:05:45PM => 19:05:45", func() {
+		input := "07:05:45PM"
+		expected := "19:05:45"
+
+		actual := TimeConversion(input)
+
+    Expect(actual).To(Equal(expected))
+	})
+})
